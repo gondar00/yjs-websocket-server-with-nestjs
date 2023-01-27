3c8d975e7d867686073d2a1de3e0953c103fe78c
gandharv garg <gandharv@gandharvs-MacBook-Air.local>
2023-01-27 14:55:23 +0530

    fix: max-connections issue

--- src/yjs.gateway.ts
+++ src/yjs.gateway.ts
@@ -5,19 +5,37 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
+import { Request } from 'express';
 import { Server } from 'ws';
 import { setupWSConnection } from 'y-websocket/bin/utils';
+import { getCookie } from './utils';
 
-@WebSocketGateway()
+@WebSocketGateway({ path: '/yjs' })
 export class YjsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor() {}
 
   @WebSocketServer()
   server: Server;
 
-  handleConnection(client: any): void {
-    this.server.on('connection', setupWSConnection);
+  handleConnection(connection: WebSocket, request: Request): void {
+    // We can handle authentication of user like below
+
+    // const token = getCookie(request?.headers?.cookie, 'auth_token');
+    // const ERROR_CODE_WEBSOCKET_AUTH_FAILED = 4000;
+    // if (!token) {
+    //   connection.close(ERROR_CODE_WEBSOCKET_AUTH_FAILED);
+    // } else {
+    //   const signedJwt = this.authService.verifyToken(token);
+    //   if (!signedJwt) connection.close(ERROR_CODE_WEBSOCKET_AUTH_FAILED);
+    //   else {
+    //     const docName = getCookie(request?.headers?.cookie, 'roomName');
+    //     setupWSConnection(connection, request, { ...(docName && { docName }) });
+    //   }
+    // }
+
+    const docName = getCookie(request?.headers?.cookie, 'roomName');
+    setupWSConnection(connection, request, { ...(docName && { docName }) });
   }
 
-  handleDisconnect(client: any): void {}
+  handleDisconnect(): void {}
 }
