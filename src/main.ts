17b1236788b4f1d62c03d9c382dc16c4c88942c7
gandharv <gandharvkgarg@gmail.com>
2023-01-27 15:17:50 +0530

    fix: max-connections issue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(5001);
 }
